2d528ee7db61fb8eec9ea30ab7d7c80fbb2e3bab
TheFredward <fventura59@gmail.com>
2019-05-29 20:53:45 -0500

    first commit

---
+++ main_page.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+# create the screen from the application
+cv2.namedWindow('editor page..')
+# set necessary key to end program
+cv2.waitKey(0)
+# destroyAllWindows after key press
+cv2.destroyAllWindows()
